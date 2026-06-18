d1872d1370058d2fe6f3fea67bcb2949cbdd501a
TNKDieguexPCLinux <diegoaguilar20040303@gmail.com>
2026-06-18 14:06:49 -0400

    - Ajout des types pour la création d'utilisateur (CreationUtilisateur, UtilisateurData) et la gestion des erreurs (ErreurResponseDto)

--- src/features/auth/types/index.ts
+++ src/features/auth/types/index.ts
@@ -2,6 +2,17 @@ export interface LoginRequestDto{
     courriel: string;
     motPasse: string;
 }
+export interface CreationUtilisateur {
+    nom: string;
+    courriel: string;
+    motPasse: string;
+    motPasseConfirmation: string;
+}
+export interface UtilisateurData {
+    nom: string;
+    courriel: string;
+    motPasse: string;
+}
 export interface AuthResponseDto {
     token: string;
     utilisateur: UtilisateurDto;
@@ -11,4 +22,9 @@ export interface UtilisateurDto{
     courriel: string;
     motPasse: string;
     uuid: string;
+}
+export interface ErreurResponseDto {
+    message: string;
+    timeStamp: string;
+    status: number;
 }
