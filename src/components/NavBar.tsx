fbe11bfaf1b31daafdbfce04ac3c3e04a09c60b5
TNKDieguexPCLinux <diegoaguilar20040303@gmail.com>
2026-06-18 14:08:34 -0400

    - Ajout du bouton "Logout" dans le composant NavBar - Implémentation de la logique de déconnexion (suppression du token dans le localStorage et redirection vers '/login') - Ajout de classes utilitaires CSS pour améliorer l'alignement et la mise en page de la barre de navigation

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,20 @@ import {Link} from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <nav className="navbar">
-            <ul className="navbar-list">
+        <nav className="navbar flex justify-between items-center p-4">
+            <ul className="navbar-list flex justify-between gap-4 ">
                 <li><Link className={"navbar-link"} to={"/dashboard"}>Inicio</Link></li>
                 <li><Link className={"navbar-link"} to={"/login"}>Login</Link></li>
                 <li><Link className={"navbar-link"} to={"/register"}>Register</Link></li>
                 <li><Link className={"navbar-link"} to={"/dashboard"}>Dashboard</Link></li>
+                <li><button className={"bg-transparent border-0 cursor-pointer"}
+                            onClick={()=>{
+                                localStorage.removeItem('token');
+                                window.location.href = '/login';
+                            }}>
+                        Logout
+                    </button>
+                </li>
             </ul>
         </nav>
     )
