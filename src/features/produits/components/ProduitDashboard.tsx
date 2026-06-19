facb831a14b6f77d8bba89e809f2b26221845cff
TNKDieguexPCLinux <diegoaguilar20040303@gmail.com>
2026-06-18 23:28:04 -0400

    - Ajout de styles de mise en page au composant ProduitDashboard - Ajout d'un conteneur et d'un texte descriptif pour l'inventaire des produits

--- src/features/produits/components/ProduitDashboard.tsx
+++ src/features/produits/components/ProduitDashboard.tsx
@@ -1,7 +1,13 @@
 const ProduitDashboard = () => {
     return (
-        <div>
-            <h1>Produit Dashboard</h1>
+        <div className="space-y-4">
+            <h1 className="text-2xl font-bold text-gray-900">Produit Dashboard</h1>
+
+            <div className="bg-white p-6 rounded-xl shadow-sm border border-gray-100 h-[120vh]">
+                <p className="text-gray-600">
+                    Inventaire de produits de la famille
+                </p>
+            </div>
         </div>
     );
 };
