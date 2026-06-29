878ab60758fcd393a7951eb3b0f55a26c1cb1552
TNKDieguexPCLinux <diegoaguilar20040303@gmail.com>
2026-06-29 15:59:44 -0400

    - Remplacement du bouton "Logout" et de sa logique de déconnexion par un lien vers la page "/profil" dans le composant NavBar - Suppression du commentaire TODO concernant la création de l'espace profil

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,12 @@
 import {Link} from "react-router-dom";
 
 const NavBar = () => {
-    // TODO: apartado de "profil" para ver los datos del usuario, poder desconectarse y salirse de la familia
     return (
         <nav className="navbar navbar-body">
             <ul className="navbar-list flex flex-row gap-4 content-end">
                 <li><Link className={"navbar-link"} to={"/dashboard"}>Dashboard</Link></li>
                 <li><Link className={"navbar-link"} to={"/famille"}>Famille</Link></li>
-                <li><button className={"bg-transparent border-0 cursor-pointer"}
-                            onClick={()=>{
-                                localStorage.removeItem('token');
-                                sessionStorage.clear();
-                                window.location.href = '/login';
-                            }}>
-                        Logout
-                    </button>
-                </li>
+                <li><Link className={"navbar-link"} to={"/profil"}>Profil</Link></li>
             </ul>
         </nav>
     )
