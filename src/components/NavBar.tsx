46efee4174ac5e48537c03d0dfcfc303217c97da
TNKDieguexPCLinux <diegoaguilar20040303@gmail.com>
2026-06-30 01:24:53 -0400

    - Remplacement du composant `Link` par `NavLink` de react-router-dom - Ajout d'icônes SVG pour chaque lien de navigation (Dashboard, Famille, Profil) - Nettoyage et mise à jour des classes CSS pour les éléments de la barre de navigation

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,30 @@
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <nav className="navbar navbar-body">
-            <ul className="navbar-list flex flex-row gap-4 content-end">
-                <li><Link className={"navbar-link"} to={"/dashboard"}>Dashboard</Link></li>
-                <li><Link className={"navbar-link"} to={"/famille"}>Famille</Link></li>
-                <li><Link className={"navbar-link"} to={"/profil"}>Profil</Link></li>
+        <nav className="navbar-body">
+            <ul className="navbar-list">
+                <li>
+                    <NavLink to="/dashboard" className="navbar-link">
+                        <svg className="size-5"><use href="/sprite.svg#options" /></svg>
+                        <span>Dashboard</span>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/famille" className="navbar-link">
+                        <svg className="size-5"><use href="/sprite.svg#family" /></svg>
+                        <span>Famille</span>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/profil" className="navbar-link">
+                        <svg className="size-5"><use href="/sprite.svg#profil" /></svg>
+                        <span>Profil</span>
+                    </NavLink>
+                </li>
             </ul>
         </nav>
-    )
+    );
 }
 
 export default NavBar;
