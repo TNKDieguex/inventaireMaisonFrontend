cac73ac0d75fd6c3a0a995135fb684a8db8cae41
TNKDieguexPCLinux <diegoaguilar20040303@gmail.com>
2026-07-01 15:21:33 -0400

    - Utilisation de la variable d'environnement `VITE_API_URL` pour la configuration de la `baseURL` dans `axiosClient` (avec valeur de repli sur localhost)

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -3,7 +3,7 @@ import type {ErreurResponseDto} from "../features/auth/types";
 
 
 const axiosClient: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
